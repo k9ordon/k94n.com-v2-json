3aaeed12c6170c11b80b0323bc3b70a1a7684f76
k9ordon <k9ordon@gmail.com>
2013-07-12 16:54:05 +0200

    minify markup

--- index.php
+++ index.php
@@ -12,4 +12,10 @@
 	$jawbone = $externals->getJawboneYesterday();
 
 	// render
+	ob_start();
 	include "view.php";
+	$markup = ob_get_contents();
+	ob_end_clean();
+
+	// minify output
+	print preg_replace('#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))#', ' ', $markup);
