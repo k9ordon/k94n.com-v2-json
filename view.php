4fd639292b0c14095bd0d66782aa6432b55cf86d
k9ordon <k9ordon@gmail.com>
2013-07-12 15:42:22 +0200

    responsive

--- view.php
+++ view.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>Hello! My name is Klemens Gordon - k94n.com</title>
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="assets/built/css/app.css">
     <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -11,15 +15,15 @@
 
 <body>
 	<div class="wrap">
-		<h1>Hello! <span>My name is <a href="http://twitter.com/thisisgordon">Klemens Gordon</a></span></h1>
+		<h1>Hello! <span>My name is <a href="http://twitter.com/thisisgordon">Klemens Gordon</a>.</span></h1>
 		<h2>I am a webdeveloper with love for interfaces.</h2>
 		<h3>I currently code at <a href="http://dev.karriere.at">karriere.at</a>.</h3>
 		<h3>I enjoy listening to <a href="http://last.fm/user/mracidfreak"><?=$artists[0]['name']?></a>.</h3>
-		<h3>I recorded <?=$jawbone['steps']?> steps yesterday.</h3>
+		<h3>I moved <?=$jawbone['steps']?> steps yesterday.</h3>
 
 		<div id="footprint">
 			<p>I push code to <a href="http://github.com/k9ordon">github</a>, talk on <a href="http://twitter.com/thisisgordon">twitter</a> and bookmark at <a href="http://delicious.com/k9ordon">delicious</a>.</p>
-			<p>Need code? Ninja is ready <a href="">hello@k94n.com</a></p>
+			<p>Need code? Ninja is ready <a href="">hello@k94n.com</a>.</p>
 		</div>
 	</div>
 
